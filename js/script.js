47614001553e10ade2813735c818f7a2c01df201
Yoonseayoung <yoondinosour@naver.com>
2022-03-20 23:42:03 +0900

    프로젝트3(카카오게임즈) 첫번째 커밋입니다.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+/* slide */
+$(document).ready(function(){
+    $('.content').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      speed: 2000,
+      dots: true,
+    });
+});
